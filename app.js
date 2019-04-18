954df883d09bd6ea31db38a33e8453177dd5e3cc
s2010 <bashayer.hijji@gmail.com>
2019-04-18 18:12:52 +0300

    fixing POST endpoint

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-// Importing Express.js module
 const express = require('express');
 const bodyParser = require('body-parser');
-// Importing Blockchain class (aka simpleChain.js in Project2)
 const BlockchainService = require('./blockchain.js');
-// Importing Blockchain service
 const BlockController = require('./blockController.js');
-// Importing BlockchainData class to be used as wrapper for writing on level DB
 const BlockchainData = require('./blockchainData.js');
 
 // initialize express app
 const app = express();
 const port = 8000;
 
-
-// Insantiating classes - Creating NVC patterns for chaining
+// Insantiating 
 let blockchainData = new BlockchainData("./chaindata");
 let blockchainService = new BlockchainService(blockchainData);
 let blockController = new BlockController(blockchainService);
@@ -21,12 +16,10 @@ let blockController = new BlockController(blockchainService);
 // setting the port
 app.set("port", port);
 
-// for parsing application/json
 app.use(bodyParser.json());
-// for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/block/:blockIndex', blockController.getBlockByIndex.bind(blockController));
+app.get('/block/:index', blockController.getBlockByIndex.bind(blockController));
 app.post('/block', blockController.postNewBlock.bind(blockController));
 
 // 2nd Rubric Point: API Service Port Configuration - use port 8000 with localhost URL
--- blockController.js
+++ blockController.js
@@ -5,21 +5,18 @@ const Block = require('./block.js');
  * Controller Definition to encapsulate routes to work with blocks
  */
 class BlockController {
-
-    // Modified constructor to mimic only acted based on what has been exposed by BlockChain Service
     constructor(blockchainService) {
         this.blockchainService = blockchainService;
     }
 
     /**
-       Rubric Point 3-A Configure End Point - GET
-     * Implement a wrapper to GET Endpoint to retrieve a block by index, url: "/api/block/:index"
+       End Point - GET
      */
     getBlockByIndex(req, res) {
-      let idx = req.params.blockIndex;
-      console.log('index is:   ' + idx);
-      let blockheight_prom = this.blockchainService.getBlock(idx);
-      blockheight_prom.then(block => {
+      let idx = req.params.index;
+      console.log('index is:  ' + idx);
+      let blockheight_issue = this.blockchainService.getBlock(idx);
+      blockheight_issue.then(block => {
         res.send(block);
       }).catch(err => {
         return res.status(500).send("Invalid Block: This Block Number is not present in the chain");   // bad request with error code
@@ -27,8 +24,7 @@ class BlockController {
     }
 
     /**
-       Rubric Point 3-B Configure End Point - POST
-     * Implement a wrapper to POST Endpoint Service to add a new Block, url: "/api/block"
+       End Point - POST
      */
     postNewBlock(req, res) {
       let bodydata = req.body.data;
